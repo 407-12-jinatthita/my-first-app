5e262c8acb1c83d0ad8e17ee15978ad77cd34b87
407-12-jinatthita <54212@yupparaj.ac.th>
2026-08-21 12:09:49 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -51,13 +51,13 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
     else:
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
     # ตรวจข้อ 3
-    if u_ans3 == "Lemon":
+    if u_ans3 == "lemon":
         st.success("✅ ข้อ 3: ถูกต้อง")
         score += 1
     else:
         st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
     # ตรวจข้อ 4
-    if u_ans4 == "Pen":
+    if u_ans4 == "pen":
         st.success("✅ ข้อ 4: ถูกต้อง")
         score += 1
     else:
